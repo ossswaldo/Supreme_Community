fb71a1e1dc43bfd2e78b9361bdd58c6215a61f0a
Oswaldo Argueta <BigO@Oswaldos-MBP.home>
2019-08-21 18:00:57 -0500

    New code

---
+++ Thumbs_Up.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+#from easygui import*
+
+
+from datetime import datetime
+import time
+
+
+class ChangeTheMarket():
+
+    def __init__(self):
+        self.driver = None
+
+    def setupselenium(self):
+        options = Options()
+        options.add_argument('start-maximized')
+        options.add_argument('disable-infobars')
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path=r'/Users/BigO/Documents/CODE/Supreme_Community/chromedriver')
+
+    def execute(self):
+
+
+    def openUpBrowser(self):
+        print(datetime.now(),'Supreme Community Script Initiated -')
+        self.driver.get('https://www.supremecommunity.com/season/fall-winter2019/droplist/2019-08-22/')
+        print(datetime.now(),'Openeing supremecommunity.com)
+
+    def cleanUpBrowser(self):
+        print(datetime.now(),'Quitting Browser')
+        self.driver.quit()
+
+
+if __name__== "__main__":
+    taskMaster =  ChangeTheMarket()
+    taskMaster.execute()
