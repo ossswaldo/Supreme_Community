1b29eacdffd693ce255ad0c9b89c22327860bee8
Oswaldo Argueta <BigO@cometnet-10-21-24-136.utdallas.edu>
2019-08-22 10:20:08 -0500

    small changes

--- Thumbs_Up.py
+++ Thumbs_Up.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -20,18 +21,23 @@ class ChangeTheMarket():
 
     def setupselenium(self):
         options = Options()
+        #options = webdriver.ChromeOptions()
         options.add_argument('start-maximized')
         options.add_argument('disable-infobars')
-        options.add_argument('headless')
+        options.add_argument('ignore-certificate-errors')
+        options.add_argument('incognito')
+        #options.add_argument('headless')
         self.driver = webdriver.Chrome(chrome_options=options, executable_path=r'/Users/BigO/Documents/CODE/Supreme_Community/chromedriver')
 
     def execute(self):
-
+        self.setupselenium()
+        self.openUpBrowser()
+        #self.cleanUpBrowser()
 
     def openUpBrowser(self):
         print(datetime.now(),'Supreme Community Script Initiated -')
         self.driver.get('https://www.supremecommunity.com/season/fall-winter2019/droplist/2019-08-22/')
-        print(datetime.now(),'Openeing supremecommunity.com)
+        print(datetime.now(),'Openeing supremecommunity.com')
 
     def cleanUpBrowser(self):
         print(datetime.now(),'Quitting Browser')
